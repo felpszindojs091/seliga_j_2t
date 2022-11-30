b1bb172e66ee369d9b5da3a1bf6c55773e535027
FelpsziNdoTERMUX <106183643+felpszindojs091@users.noreply.github.com>
2022-11-30 16:49:48 -0300

    Add files via upload

--- relogio.js
+++ relogio.js
@@ -1,1 +1,18 @@
 
+function clock(){
+    let d = new Date();
+    let h = d.getHours();    
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    let hora = h + ":" + m + ":" + s;
+    if (h < 10){
+        let hora = "0" + h + ":" + m + ":" + s;
+    }
+if (m < 10){
+    let hora = h + ":0" + m + ":" + s;
+    if (s < 10){
+        let hora = h + ":" + m + ":0" + s;
+    }
+}
+
+}
